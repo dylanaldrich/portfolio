a6e99ec4421373b1f08decc9dfbd060596c57c38
dylanaldrich <aldrich.dylan@gmail.com>
2020-11-16 15:20:09 -0500

    adds navbar and header

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import '../App.css';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <header className="jumbotron d-flex align-items-end">
+                <div className="header-text mx-auto text-white mt-3">
+                    <h1 className="display-4 font-weight-bold">Dylan Aldrich</h1>
+                    <p className="lead font-weight-bold">Software Engineer</p>
+                </div>
+            </header>
+        </div>
+    )
+};
+
+export default LandingPage;
