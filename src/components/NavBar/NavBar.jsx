03d9a5d999ebfb81dcd58479fd31107fbbecc8de
dylanaldrich <aldrich.dylan@gmail.com>
2020-11-17 14:02:44 -0500

    adds frame for skills section

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-lg">
             {/* navbar-light bg-light */}
             <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
+                <i className="fas fa-bars"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav">
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -137,7 +137,7 @@ const LandingPage = () => {
                 </div>
 
                 {/* Song Quiz Section */}
-                <div className="project-container d-flex align-items-center text-white mb-5" id="song-quiz-section">
+                <div className="project-container d-flex align-items-center text-white mb-2" id="song-quiz-section">
                     <div className="project container d-flex align-items-center">
                         <a href="https://pages.git.generalassemb.ly/dylanaldrich/dylans-song-quiz/" target="_blank" rel="noreferrer">
                             <img className="shadow project-img" src="https://i.ibb.co/rv6KtcK/songquiz-home.png" alt="song quiz" />
@@ -165,6 +165,13 @@ const LandingPage = () => {
                 </div>
             </section>
 
+            {/* Skills Section */}
+            <section id="skills">
+                <h2 className="font-weight-bold my-4 text-white section-title">SKILLS</h2>
+                <div className="project-container d-flex align-items-center text-white mb-5" id="skills-section">
+                    Skills go here
+                </div>
+            </section>
 
             {/* Footer */}
             <footer className="footer mt-auto">
