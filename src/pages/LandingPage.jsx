187bfe8e8776ef74d4014b225d085293bca25e56
Dylan Aldrich <dylan@sonosim.com>
2024-09-28 09:41:46 -0600

    Updates dependencies and tidies up landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,10 +5,10 @@ import cv_photo from '../assets/images/cv_photo.png';
 import {skillsLogos} from '../variables/consts';
 
 const LandingPage = () => {
-    
+
     function generateSkills(array) {
-        return array.map((skill) => {
-            return (<i className={`m-2 mb-3 ${skill}`} key={array.indexOf(skill)}></i>);
+        return array.map((skill, index) => {
+            return (<i data-for="main" data-tip={skill.name} className={`m-2 mb-3 ${skill.icon}`} key={index}></i>);
         });
     }
 
@@ -53,7 +53,7 @@ const LandingPage = () => {
                 <div className="project-container d-flex align-items-center text-white mb-5" id="agotb-section">
                     <div className="project container d-flex align-items-center">
                         <a href="https://www.terribleblooms.net" target="_blank" rel="noreferrer">
-                            <img className="shadow project-img" src="https://i.ibb.co/Pxkjg1p/terribleblooms-home.png" alt="A Garden of Terrible Blooms Homepage" />
+                            <img className="shadow project-img img-grow" src="https://i.ibb.co/Pxkjg1p/terribleblooms-home.png" alt="A Garden of Terrible Blooms Homepage" />
                         </a>
                         <div className="text-left my-3">
                             <h2 className="font-weight-bold project-title">
@@ -68,13 +68,13 @@ const LandingPage = () => {
                             </p>
                             <p className="mb-2 font-weight-bold">Technologies used:</p>
                             <div className="logo-container d-flex justify-content-between project-tech-logos">
-                                <i title="Python" className="devicon-python-plain"></i>
-                                <i title="Django" className="devicon-django-plain"></i>
-                                <i title="PostgreSQL" className="devicon-postgresql-plain"></i>
-                                <i title="jQuery" className="devicon-jquery-plain"></i>
-                                <i title="Heroku" className="devicon-heroku-original"></i>
-                                <i title="HTML" className="devicon-html5-plain-wordmark"></i>
-                                <i title="CSS" className="devicon-css3-plain-wordmark"></i>
+                                <i data-for="main" data-tip="Python" className="devicon-python-plain"></i>
+                                <i data-for="main" data-tip="Django" className="devicon-django-plain"></i>
+                                <i data-for="main" data-tip="PostgreSQL" className="devicon-postgresql-plain"></i>
+                                <i data-for="main" data-tip="jQuery" className="devicon-jquery-plain"></i>
+                                <i data-for="main" data-tip="Heroku" className="devicon-heroku-original"></i>
+                                <i data-for="main" data-tip="HTML" className="devicon-html5-plain-wordmark"></i>
+                                <i data-for="main" data-tip="CSS" className="devicon-css3-plain-wordmark"></i>
                             </div>
                         </div>
                     </div>
@@ -83,31 +83,24 @@ const LandingPage = () => {
                 {/* foodbook Section */}
                 <div className="project-container d-flex align-items-center text-white mb-5" id="foodbook-section">
                     <div className="project container d-flex align-items-center">
-                        <a href="https://foodbook-recipeapp.herokuapp.com/" target="_blank" rel="noreferrer">
-                            <img className="shadow project-img" src="https://i.ibb.co/R9yRF0D/foodbook-home.png" alt="foodbook" />
-                        </a>
+                        <img className="shadow project-img" src="https://i.ibb.co/R9yRF0D/foodbook-home.png" alt="foodbook" />
                         <div className="text-left my-3">
-                            <h2 className="font-weight-bold project-title">
-                                <a href="https://foodbook-recipeapp.herokuapp.com/" className="project-link" target="_blank" rel="noreferrer">foodbook</a>
-                            </h2>
+                            <h2 className="font-weight-bold project-title">foodbook</h2>
                             <p className="project-description mb-3">
                                 A chef's best friend! This multi-server app is linked to an <a href="https://developer.edamam.com/" className="project-link font-weight-bold" target="_blank" rel="noreferrer">external API</a> that allows you to search through a database of millions of recipes, from all over the web. You can then save your favorite recipes into as many of your own personalized "foodbooks" as you like. Once the recipes are stored, you can open your foodbooks and easily find the right recipe, for the right occasion.
                             </p>
-                            <p className="mb-3 h5 font-weight-bold">
-                                <a href="https://foodbook-recipeapp.herokuapp.com/" target="_blank" rel="noreferrer" className="btn btn-light">View</a>
-                                <a href="https://github.com/dylanaldrich/foodbook_client" target="_blank" rel="noreferrer" className="btn btn-light">GitHub</a>
-                            </p>
+                            <a href="https://github.com/dylanaldrich/foodbook_client" target="_blank" rel="noreferrer" className="btn btn-light mb-3 h5 font-weight-bold">GitHub</a>
                             <p className="mb-2 font-weight-bold">Technologies used:</p>
                             <div className="logo-container d-flex justify-content-between project-tech-logos">
-                                <i title="JavaScript" className="devicon-javascript-plain"></i>
-                                <i title="React" className="devicon-react-original"></i>
-                                <i title="Node.js" className="devicon-nodejs-plain"></i>
-                                <i title="Express" className="devicon-express-original"></i>
-                                <i title="MongoDB" className="devicon-mongodb-plain"></i>
-                                <i title="Heroku" className="devicon-heroku-original"></i>
-                                <i title="Bootstrap" className="devicon-bootstrap-plain"></i>
-                                <i title="HTML" className="devicon-html5-plain-wordmark"></i>
-                                <i title="CSS" className="devicon-css3-plain-wordmark"></i>
+                                <i data-for="main" data-tip="JavaScript" className="devicon-javascript-plain"></i>
+                                <i data-for="main" data-tip="React" className="devicon-react-original"></i>
+                                <i data-for="main" data-tip="Node.js" className="devicon-nodejs-plain"></i>
+                                <i data-for="main" data-tip="Express" className="devicon-express-original"></i>
+                                <i data-for="main" data-tip="MongoDB" className="devicon-mongodb-plain"></i>
+                                <i data-for="main" data-tip="Heroku" className="devicon-heroku-original"></i>
+                                <i data-for="main" data-tip="Bootstrap" className="devicon-bootstrap-plain"></i>
+                                <i data-for="main" data-tip="HTML" className="devicon-html5-plain-wordmark"></i>
+                                <i data-for="main" data-tip="CSS" className="devicon-css3-plain-wordmark"></i>
                             </div>
                         </div>
                     </div>
@@ -127,13 +120,13 @@ const LandingPage = () => {
                             </p>
                             <p className="mb-2 font-weight-bold">Technologies used:</p>
                             <div className="logo-container d-flex justify-content-between project-tech-logos">
-                                <i title="Python" className="devicon-python-plain"></i>
-                                <i title="Django" className="devicon-django-plain"></i>
-                                <i title="PostgreSQL" className="devicon-postgresql-plain"></i>
-                                <i title="Bootstrap" className="devicon-bootstrap-plain"></i>
-                                <i title="Heroku" className="devicon-heroku-original"></i>
-                                <i title="HTML" className="devicon-html5-plain-wordmark"></i>
-                                <i title="CSS" className="devicon-css3-plain-wordmark"></i>
+                                <i data-for="main" data-tip="Python" className="devicon-python-plain"></i>
+                                <i data-for="main" data-tip="Django" className="devicon-django-plain"></i>
+                                <i data-for="main" data-tip="PostgreSQL" className="devicon-postgresql-plain"></i>
+                                <i data-for="main" data-tip="Bootstrap" className="devicon-bootstrap-plain"></i>
+                                <i data-for="main" data-tip="Heroku" className="devicon-heroku-original"></i>
+                                <i data-for="main" data-tip="HTML" className="devicon-html5-plain-wordmark"></i>
+                                <i data-for="main" data-tip="CSS" className="devicon-css3-plain-wordmark"></i>
                             </div>
                         </div>
                     </div>
@@ -153,15 +146,15 @@ const LandingPage = () => {
                             </p>
                             <p className="mb-2 font-weight-bold">Technologies used:</p>
                             <div className="logo-container d-flex justify-content-between project-tech-logos">
-                                <i title="JavaScript" className="devicon-javascript-plain"></i>
-                                <i title="Node.js" className="devicon-nodejs-plain"></i>
-                                <i title="Express" className="devicon-express-original"></i>
-                                <i title="MongoDB" className="devicon-mongodb-plain"></i>
-                                <i title="jQuery" className="devicon-jquery-plain"></i>
-                                <i title="Bootstrap" className="devicon-bootstrap-plain"></i>
-                                <i title="Heroku" className="devicon-heroku-original"></i>
-                                <i title="HTML" className="devicon-html5-plain-wordmark"></i>
-                                <i title="CSS" className="devicon-css3-plain-wordmark"></i>
+                                <i  data-for="main" data-tip="JavaScript" className="devicon-javascript-plain"></i>
+                                <i  data-for="main" data-tip="Node.js" className="devicon-nodejs-plain"></i>
+                                <i  data-for="main" data-tip="Express" className="devicon-express-original"></i>
+                                <i  data-for="main" data-tip="MongoDB" className="devicon-mongodb-plain"></i>
+                                <i  data-for="main" data-tip="jQuery" className="devicon-jquery-plain"></i>
+                                <i  data-for="main" data-tip="Bootstrap" className="devicon-bootstrap-plain"></i>
+                                <i  data-for="main" data-tip="Heroku" className="devicon-heroku-original"></i>
+                                <i  data-for="main" data-tip="HTML" className="devicon-html5-plain-wordmark"></i>
+                                <i  data-for="main" data-tip="CSS" className="devicon-css3-plain-wordmark"></i>
                             </div>
                         </div>
                     </div>
@@ -171,7 +164,7 @@ const LandingPage = () => {
                 <div className="project-container d-flex align-items-center text-white mb-2" id="song-quiz-section">
                     <div className="project container d-flex align-items-center">
                         <a href="https://pages.git.generalassemb.ly/dylanaldrich/dylans-song-quiz/" target="_blank" rel="noreferrer">
-                            <img className="shadow project-img" src="https://i.ibb.co/rv6KtcK/songquiz-home.png" alt="song quiz" />
+                            <img className="shadow project-img img-grow" src="https://i.ibb.co/rv6KtcK/songquiz-home.png" alt="song quiz" />
                         </a>
                         <div className="text-left my-3">
                             <h2 className="font-weight-bold project-title">
@@ -186,10 +179,10 @@ const LandingPage = () => {
                             </p>
                             <p className="mb-2 font-weight-bold">Technologies used:</p>
                             <div className="logo-container d-flex justify-content-between project-tech-logos">
-                                <i title="JavaScript" className="devicon-javascript-plain"></i>
-                                <i title="jQuery" className="devicon-jquery-plain"></i>
-                                <i title="HTML" className="devicon-html5-plain-wordmark"></i>
-                                <i title="CSS" className="devicon-css3-plain-wordmark"></i>
+                                <i data-for="main" data-tip="JavaScript" className="devicon-javascript-plain"></i>
+                                <i data-for="main" data-tip="jQuery" className="devicon-jquery-plain"></i>
+                                <i data-for="main" data-tip="HTML" className="devicon-html5-plain-wordmark"></i>
+                                <i data-for="main" data-tip="CSS3" className="devicon-css3-plain-wordmark"></i>
                             </div>
                         </div>
                     </div>
@@ -200,9 +193,7 @@ const LandingPage = () => {
             <section id="skills">
                 <h2 className="font-weight-bold my-4 text-white section-title">SKILLS</h2>
                 <div className="d-flex flex-column align-items-center text-white mb-1 py-3" id="skills-section">
-                    <div className="skills-grid d-flex flex-wrap justify-content-center">
-                    {generateSkills(skillsLogos)}
-                    </div>
+                    <div className="skills-grid d-flex flex-wrap justify-content-center">{generateSkills(skillsLogos)}</div>
                     <div className="pt-2 d-flex align-items-center">
                         <h4>Languages:</h4>
                         <h2 className="h1 ml-2" id="languages">
@@ -212,13 +203,13 @@ const LandingPage = () => {
                                 className="flag mr-2">
                                     &#127468;&#127463;
                             </span>
-                            <span 
+                            <span
                                 data-for="main"
                                 data-tip="Español"
                                 className="flag mr-2">
                                     &#127466;&#127480;
                             </span>
-                            <span 
+                            <span
                                 data-for="main"
                                 data-tip="Français"
                                 className="flag">
